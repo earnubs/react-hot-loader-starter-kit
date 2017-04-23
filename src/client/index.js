6e98558b9ef686f3fcc73125b4062d7a43cd58c8
Stephen Stewart <stephen.stewart@carisenda.com>
2017-04-23 12:31:12 +0100

    add react-router-redux bindings

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,18 @@
-import { AppContainer } from 'react-hot-loader';
 import React from 'react';
+import createHistory from 'history/createBrowserHistory';
+import thunk from 'redux-thunk';
+import { AppContainer } from 'react-hot-loader';
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { render } from 'react-dom';
 
 import App from './containers/app';
-import reducers from './reducers';
+import rootReducer from './reducers';
+
+const history = createHistory();
+const routing = routerMiddleware(history);
+
 
 // Grab the state from a global variable injected into the server-generated HTML
 const preloadedState = window.__PRELOADED_STATE__;
@@ -14,17 +21,23 @@ const preloadedState = window.__PRELOADED_STATE__;
 const store = (module.hot && module.hot.data && module.hot.data.store)
   ? module.hot.data.store
   : createStore(
-  reducers, preloadedState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    rootReducer,
+    preloadedState,
+    compose(
+      applyMiddleware(thunk, routing),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 );
 
 const contentEl = document.getElementById('content');
 
 render(
   <Provider store={store}>
-    <AppContainer>
-      <App />
-    </AppContainer>
+    <ConnectedRouter history={history}>
+      <AppContainer>
+        <App />
+      </AppContainer>
+    </ConnectedRouter>
   </Provider>,
   contentEl
 );
