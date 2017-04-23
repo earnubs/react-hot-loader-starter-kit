d3decddb6781fa3dab19ac76cb09a0c6ef45bb0e
Stephen Stewart <stephen.stewart@carisenda.com>
2017-04-23 12:28:08 +0100

    add actions and reducers to handle requests for fx data

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,12 +1,102 @@
-// action types
+import fetch from 'isomorphic-fetch';
 
-export const SAY_HELLO = 'SAY_HELLO';
+const DEFAULT_SYMBOLS = [
+  'USD',
+  'EUR',
+  'JPY',
+  'GBP',
+  'AUD',
+  'CAD',
+  'CHF',
+  'CNY',
+  'SEK',
+  'NZD'
+];
 
-// action creators
+export const REQUEST_RATES = 'REQUEST_RATES';
+export const REQUEST_RATES_FAILURE = 'REQUEST_RATES_FAILURE';
 
-export function sayHello(who) {
+export function requestRates() {
   return {
-    type: SAY_HELLO,
-    who
+    type: REQUEST_RATES
+  };
+}
+
+export const REQUEST_RATES_SUCCESS = 'REQUEST_RATES_SUCCESS';
+export function requestRatesSuccess(json) {
+  return {
+    type: REQUEST_RATES_SUCCESS,
+    payload: json
+  };
+}
+
+export function fetchRates() {
+  return function (dispatch) {
+    dispatch(requestRates());
+
+    return fetch(`http://api.fixer.io/latest?symbols=${DEFAULT_SYMBOLS}`)
+      .then(response => response.json())
+      .then(json =>
+        dispatch(requestRatesSuccess(json))
+      );
+
+    // TODO handle failure
+  };
+}
+
+export const UPDATE_BASE_AMOUNT = 'UPDATE_BASE_AMOUNT';
+export function updateBaseAmount(amount) {
+  return {
+    type: UPDATE_BASE_AMOUNT,
+    payload: amount
+  };
+}
+
+export const SELECT_QUOTE = 'SELECT_QUOTE';
+export function selectQuote(quote) {
+  return {
+    type: SELECT_QUOTE,
+    payload: quote
+  };
+}
+
+export const CLEAR_SELECTED_QUOTES= 'CLEAR_SELECTED_QUOTES';
+export function clearSelectedQuotes() {
+  return {
+    type: CLEAR_SELECTED_QUOTES
+  };
+}
+
+export const REQUEST_5DAY_RATES = 'REQUEST_5DAY_RATES';
+export function request5DayRates() {
+  return {
+    type: REQUEST_5DAY_RATES
+  };
+}
+
+export function fetch5DayRates(symbols) {
+  return function (dispatch) {
+    dispatch(request5DayRates);
+
+    // TODO use momentjs to figure out correct dates, rates are updated "daily daily around 4PM CET"
+    // TODO working days only
+    const dates = ['2017-04-21', '2017-04-20', '2017-04-19', '2017-04-18', '2017-04-13'];
+
+    return Promise.all(dates.reverse().map(date => {
+      return fetch(`http://api.fixer.io/${date}?symbols=${symbols.join()}`)
+        .then(response => response.json());
+    }))
+      .then(json => {
+        dispatch(request5DayRatesSuccess(json));
+      });
+  };
+}
+
+export const REQUEST_5DAY_RATES_SUCCESS = 'REQUEST_5DAY_RATES_SUCCESS';
+export function request5DayRatesSuccess(json) {
+  return {
+    type: REQUEST_5DAY_RATES_SUCCESS,
+    payload: json
+    // TODO receivedAt, only check if 24 hrs have passed or after 4PM CET
   };
 }
--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,23 +1,83 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
 
-import * as actions from '../actions';
+import * as actionTypes from '../actions';
 
 const rootReducer = combineReducers({
-  sayStuff
+  latest,
+  historic,
+  routing: routerReducer
 });
 
-export function sayStuff(state={}, action) {
+export function latest(state = {
+  amount: 1,
+  isFetching: false,
+  error: null,
+  selected: [],
+  rates: {}
+}, action) {
   switch (action.type) {
-    case actions.SAY_HELLO:
-      return [
+    case actionTypes.UPDATE_BASE_AMOUNT:
+      return {
         ...state,
-        {
-          to: action.who
-        }
-      ];
+        amount: action.payload
+      };
+    case actionTypes.REQUEST_RATES:
+      return {
+        ...state,
+        isFetching: true
+      };
+    case actionTypes.REQUEST_RATES_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        error: null,
+        rates: action.payload.rates
+      };
+    case actionTypes.REQUEST_RATES_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      };
+    case actionTypes.SELECT_QUOTE: {
+      return {
+        ...state,
+        selected: state.selected.concat(action.payload).slice(-2)
+      };
+    }
+    case actionTypes.CLEAR_SELECTED_QUOTES:
+      return {
+        ...state,
+        selected: []
+      };
     default:
       return state;
   }
 }
+
+export function historic(state = {
+  isFetching: false,
+  error: null,
+  dailyRates: {}
+}, action) {
+
+  switch (action.type) {
+    case actionTypes.REQUEST_5DAY_RATES:
+      return {
+        ...state,
+        isFetching: true
+      };
+    case actionTypes.REQUEST_5DAY_RATES_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        dailyRates: action.payload
+      };
+    default:
+      return state;
+  }
+}
+
 
 export default rootReducer;
