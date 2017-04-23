5eb42dc128d0430d9fc983a11c9c59a92b85bb44
Stephen Stewart <stephen.stewart@carisenda.com>
2017-04-23 13:56:04 +0100

    fix jest eslint config for jest globals and include recommended jest rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
   'env': {
     'browser': true,
     'es6': true,
-    'node': true
+    'node': true,
+    'jest/globals': true
   },
   'extends': [
     'eslint:recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:jest/recommended'
   ],
   'parserOptions': {
     'ecmaFeatures': {
