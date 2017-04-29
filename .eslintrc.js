af97e1c9e0d276b356bdfc5a0dc2e978a7840b8a
Stephen Stewart <stephen.stewart@carisenda.com>
2017-04-29 11:50:28 +0100

    use jest recommended lints

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   },
   'extends': [
     'eslint:recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:jest/recommended'
   ],
   'parserOptions': {
     'ecmaFeatures': {
