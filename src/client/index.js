6c0e18786ff4b1a43c60dc4e19ee1d2ae5478bbd
Stephen Stewart <stephen.stewart@carisenda.com>
2017-10-05 20:58:07 +0100

    hydrate, not deprecated render

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,7 @@ import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { render } from 'react-dom';
+import { hydrate } from 'react-dom';
 
 import App from './containers/app';
 import reducers from './reducers';
@@ -20,7 +20,7 @@ const store = (module.hot && module.hot.data && module.hot.data.store)
 
 const contentEl = document.getElementById('content');
 
-render(
+hydrate(
   <Provider store={store}>
     <AppContainer>
       <App />
